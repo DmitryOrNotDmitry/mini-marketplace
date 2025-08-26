c5a514f490afd45534bf1677a5553bc1eb0696f3
Dmitry1232321 <cuznetsov.dmitri2003@yandex.ru>
2025-08-26 22:27:27 +0300

    [HW-1] Захардкодил путь к values_ci.yaml

--- cart/cmd/cart/main.go
+++ cart/cmd/cart/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	app, err := app.NewApp(os.Getenv("ROUTE_256_WS1_CONFIG"))
+	cfg := os.Getenv("ROUTE_256_WS1_CONFIG")
+	if cfg == "" {
+		cfg = "./configs/values_ci.yaml"
+	}
+	app, err := app.NewApp(cfg)
 	if err != nil {
 		panic(err)
 	}
